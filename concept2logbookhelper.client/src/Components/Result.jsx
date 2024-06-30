e8565a59b1e236ac81e6b9ff5ee3abe7866805ad
Rebecca <rebeayto@gmail.com>
2024-06-30 12:33:57 +0100

    fixed time/ distance order between headers and results

--- concept2logbookhelper.client/src/Components/Result.jsx
+++ concept2logbookhelper.client/src/Components/Result.jsx
@@ -5,8 +5,8 @@ function Result({date, type_pretty, type, distance, time, pace, spm, calories}) 
       <tr>
           <td>{moment(date).format('YYYY-MM-DD HH:mm')}</td>
           <td><div className={'workout_type ' + type}>{type_pretty}</div></td>
-          <td>{distance}m</td>
           <td>{time}</td>
+          <td>{distance}m</td>
           <td>{pace}</td>
           <td>{spm}</td>
           <td>{calories}</td>
