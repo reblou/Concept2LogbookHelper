4451f2a7b61b6ff3448950b61c119949a4559b67
Rebecca <rebeayto@gmail.com>
2024-09-06 20:18:07 +0100

    login page slight reformat

--- concept2logbookhelper.client/src/Components/Login.jsx
+++ concept2logbookhelper.client/src/Components/Login.jsx
@@ -9,16 +9,21 @@ function Login() {
           <title>Logbook Helper</title>
           <h1 id="tabelLabel">Logbook Helper</h1>
           <hr />
-          <h2>What is Logbook helper?</h2>
-          <p>Logbook helper is a React/ ASP.NET Core Web API web app designed to integrate with your Concept 2 workout history and present it in a more useful way.</p>
 
-
-
-          <p>It allows you to filter and sort workouts by date, workout type, time, distance, pace, SPM, calories and heart rate. Making it easy to compare past performance across similar workouts.</p>
-
-          <button onClick={Login}>Log In via Concept2</button>
-
-          <button onClick={DummyLogin} >Log In to Dummy Account</button>
+          <div> 
+              <div>
+                  <h2>What is Logbook helper?</h2>
+                  <p>Logbook helper is a React/ ASP.NET Core Web API web app designed to integrate with your Concept 2 workout history and present it in a more useful way.</p>
+                  <p>It allows you to filter and sort workouts by date, workout type, time, distance, pace, SPM, calories and heart rate. Making it easy to compare past performance across similar workouts.</p>
+                  <button onClick={Login}>Log In via Concept2</button>
+              </div>
+              <div>
+                <br/>
+                <h2>I don&apos;t have an Concept 2 account</h2>
+                <p>You can play around with some mock data here:</p>
+                <button onClick={DummyLogin} >Log In to Dummy Account</button>
+              </div>
+          </div>
       </div>
     );
 
@@ -40,7 +45,7 @@ function Login() {
         //TODO: if no access or refresh token stored, flag error message, do not navigate to logbook
         var response = await fetch('api/authentication/dummyLogIn');
 
-        navigate('/logbook');
+        if (response.ok) navigate('/logbook');
     }
 }
 
