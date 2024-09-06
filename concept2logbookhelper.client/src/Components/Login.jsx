eab83612100e4fca323e993d1cb069c86ad0b84e
Rebecca <rebeayto@gmail.com>
2024-09-06 20:32:20 +0100

    landing page text re-write

--- concept2logbookhelper.client/src/Components/Login.jsx
+++ concept2logbookhelper.client/src/Components/Login.jsx
@@ -13,15 +13,15 @@ function Login() {
           <div> 
               <div>
                   <h2>What is Logbook helper?</h2>
-                  <p>Logbook helper is a React/ ASP.NET Core Web API web app designed to integrate with your Concept 2 workout history and present it in a more useful way.</p>
+                  <p>Logbook helper is a web app designed to present your Concept 2 rowing machine workout history in a more useful way.</p>
                   <p>It allows you to filter and sort workouts by date, workout type, time, distance, pace, SPM, calories and heart rate. Making it easy to compare past performance across similar workouts.</p>
                   <button onClick={Login}>Log In via Concept2</button>
               </div>
               <div>
                 <br/>
-                <h2>I don&apos;t have an Concept 2 account</h2>
-                <p>You can play around with some mock data here:</p>
-                <button onClick={DummyLogin} >Log In to Dummy Account</button>
+                <h2>&quot;I don&apos;t have an Concept 2 account&quot;</h2>
+                <p>You can play around with some mock data here</p>
+                <button onClick={DummyLogin} >View Dummy Account</button>
               </div>
           </div>
       </div>
