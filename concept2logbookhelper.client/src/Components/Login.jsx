346d3d3488dbe6df44eb36837e31aeaa7b5b6549
Rebecca <rebeayto@gmail.com>
2025-01-19 14:35:08 +0000

    Landing page wording changes

--- concept2logbookhelper.client/src/Components/Login.jsx
+++ concept2logbookhelper.client/src/Components/Login.jsx
@@ -15,16 +15,16 @@ function Login() {
 
           <div> 
               <div>
-                  <h2>What is Logbook helper?</h2>
+                  <h2>About</h2>
                   <p>Logbook helper is a web app designed to present your Concept 2 rowing machine workout history in a more useful way.</p>
                   <p>It allows you to filter and sort workouts by date, workout type, time, distance, pace, SPM, calories and heart rate. Making it easy to compare past performance across similar workouts.</p>
                   <button onClick={Login}>Log In via Concept2</button>
               </div>
               <div>
                 <br/>
-                <h2>&quot;I don&apos;t have an Concept 2 account&quot;</h2>
+                <h2>Try It Without A Concept2 Account</h2>
                 <p>You can play around with some mock data here</p>
-                <button onClick={DummyLogin} >View Dummy Account</button>
+                <button onClick={DummyLogin}> View Dummy Account</button>
               </div>
           </div>
       </div>
