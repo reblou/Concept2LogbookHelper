f10e210f424f66f6d4b1612ed0eef0ad50676481
Rebecca <rebeayto@gmail.com>
2025-03-29 17:16:32 +0000

    Basic Custom tooltip on result graph showing more result info on hover

--- concept2logbookhelper.client/src/Components/Visualisations/ResultGraph.jsx
+++ concept2logbookhelper.client/src/Components/Visualisations/ResultGraph.jsx
@@ -1,4 +1,4 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import "../../css/ResultGraph.css";
 import { useState, useEffect } from 'react';
 import moment from 'moment';
@@ -24,13 +24,32 @@ function ResultGraph({ resultsInView }) {
         setPaceMax(resultsInView.reduce((max, result) => { return result.average_pace > max ? result.average_pace : max }, 0));
     }, [resultsInView]);
 
+
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active && payload && payload.length) {
+            let result = payload[0].payload;
+
+            return (
+                <div className="custom-tooltip">
+                    <p className="label">{`${moment(result.date_utc).format('YYYY-MM-DD HH:mm')} - ${result.pretty_workout_type}`}</p>
+                    <p className="pace">{`Pace: ${result.pretty_average_pace}`}</p>
+                    <p className="time">{`Time: ${result.time_formatted}`}</p>
+                    <p className="distance">{`Distance: ${result.total_distance}m`}</p>
+                </div>
+            );
+        }
+
+        return null;
+    };
+
     const renderLineChart = (
         <div className="lineChartContainer">
             <LineChart width={600} height={300} data={resultsInView}>
 				<Line type="monotone" dataKey="average_pace" stroke="#8884d8" />
 				<CartesianGrid stroke="#ccc" />
-                <XAxis dataKey="date_utc" reversed={true} tickFormatter={d => moment(d).format('YYYY-MM-DD')} />
+                <XAxis dataKey="date_utc" reversed={true} tickFormatter={d => moment(d).format('YYYY-MM-DD')} angle={45} />
                 <YAxis tickFormatter={formatPace} scale={"auto"} allowDataOverflow={true} domain={[paceMin-bufferSecs, paceMax+bufferSecs]} />
+                <Tooltip content={<CustomTooltip />} />
 			</LineChart>
 		</div>
 
